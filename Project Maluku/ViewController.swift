0873c6f48f1b3ad9032bc27783af5930291a2f1f
Andimas Muhammad Bagaswara <andimas.bagaswara@gmail.com>
2020-03-10 20:10:25 +0700

    Initial Commit

---
+++ Project Maluku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Maluku
+//
+//  Created by Andimas Bagaswara on 10/03/20.
+//  Copyright © 2020 Andimas Bagaswara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
